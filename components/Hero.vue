b914dc912e284e78beff89c7726699d2cca2a1dd
jane <janeluciaschoenfeld@gmail.com>
2025-04-19 18:25:07 +0200

    FIX picture

--- components/Hero.vue
+++ components/Hero.vue
@@ -11,7 +11,7 @@
           <AddModal button-class="btn-lg w-1/2 self-end sm:self-start" id="hero-modal" />
         </div>
         <div class="flex flex-col gap-2w-full items-end">
-          <NuxtImg class="w-full sm:w-1/2 h-full object-cover rounded-lg" src="img/hero-pic.png" alt="Hero Image" />
+          <NuxtImg class="w-full h-full object-cover rounded-lg" src="img/hero-pic.png" alt="Hero Image" />
           <div class="tooltip hover:tooltip-open tooltip-left tooltip-info absolute" data-tip="This image is AI generated.">
             <button class="btn btn-info">i</button>
           </div>
