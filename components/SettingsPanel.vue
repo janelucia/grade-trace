ebd0bed9e4723a1d2603d2e805695be749fe1063
jane <janeluciaschoenfeld@gmail.com>
2025-06-05 14:05:41 +0200

    mod: update settingsPanel

--- components/SettingsPanel.vue
+++ components/SettingsPanel.vue
@@ -6,15 +6,15 @@
       </div>
       <div class="flex items-center gap-2">
         <label class="w-full">Total Number of Semesters</label>
-        <input type="number" v-model.number="settings.totalSemesters" class="input input-sm w-32 text-right" />
+        <input type="number" v-model.number="settings.totalSemesters" class="input input-sm w-32 text-right px-6" />
       </div>
       <div class="flex items-center gap-2">
         <label class="w-full">Total ECTS Credits</label>
-        <input type="number" v-model.number="settings.totalEcts" class="input input-sm w-32 text-right" />
+        <input type="number" v-model.number="settings.totalEcts" class="input input-sm w-32 text-right px-6" />
       </div>
       <div v-if="!onboarding" class="flex items-center gap-2">
         <label class="w-full">Set own Prediction (optional)</label>
-        <input type="number" v-model.number="settings.ownPrediction" class="input input-sm w-32 text-right" />
+        <input type="number" v-model.number="settings.ownPrediction" class="input input-sm w-32 text-right px-6" />
         <Button v-if="settings.ownPrediction > 0" @click="settings.ownPrediction = 0" class="btn-secondary btn-sm">Reset</Button>
       </div>
     </div>
